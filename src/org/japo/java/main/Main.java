6940e105674c8b978463744a0c69f831dbfd1a18
Andrés Cosín Zamarreño <Cousin183@gmail.com>
2019-11-22 13:09:20 +0100

    primer commit

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,53 @@
+/* 
+ * Copyright 2019 ANDRÉS COSÍN ZAMARREÑO - Cousin183@gmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ANDRÉS COSÍN ZAMARREÑO - Cousin183@gmail.com
+ */
+public class Main {
+
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+
+    public static void main(String[] args) {
+        boolean valor = false;
+
+        boolean control = true;
+
+        do {
+
+            try {
+                System.out.print("Introduzca un valor lógico (true/false)...: ");
+                valor = SCN.nextBoolean();
+                control = false;
+
+            } catch (Exception e) {
+                System.out.println("Error");
+            } finally {
+                SCN.nextLine();
+            }
+        } while (control);
+
+        System.out.printf("El valor es ...: %b%n", valor);
+    }
+
+}
